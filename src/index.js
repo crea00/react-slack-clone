00284025ad0146d75e132d3e7b65fbf497e136cc
Jerome <654321su@gmail.com>
2018-12-17 11:07:33 +0900

    #06 Create Register Form
     - Install two libraries: semantic-ui-react, semantic-ui-css
     - Create Register Form using semantic ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => (
   <Router>
