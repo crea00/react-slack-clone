846c3029e4c483cb0c216af5a76a78f109f5eba5
Jerome <654321su@gmail.com>
2018-12-20 11:05:47 +0900

    #15 Scaffold App Components

---
+++ src/components/MetaPanel/MetaPanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MetaPanel extends Component {
+  render() {
+    return (
+      <div>MetaPanel</div>
+    )
+  }
+}
+
+export default MetaPanel;
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SidePanel extends Component {
+  render() {
+    return (
+      <div>SidePanel</div>
+    )
+  }
+}
+
+export default SidePanel;
