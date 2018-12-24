aad7b469653aec97c06fa3b3e07c7d793e16b654
Jerome <654321su@gmail.com>
2018-12-24 09:23:12 +0900

    #30 Display Messages, Create Message Component

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Comment } from 'semantic-ui-react';
+import moment from 'moment';
+
+const isOwnMessage = (message, user) => {
+  return message.user.id === user.uid ? 'message__self' : '';
+};
+
+const timeFromNow = timestamp => moment(timestamp).fromNow();
+
+const message = ({ message, user }) => (
+  <Comment>
+    <Comment.Avatar src={message.user.avatar} />
+    <Comment.Content className={isOwnMessage(message, user)}>
+      <Comment.Author as="a"></Comment.Author>
+      <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
+      <Comment.Text>{message.content}</Comment.Text>
+    </Comment.Content>
+  </Comment>
+);
+
+export default message;
