f420e48c04bc6e7942cc8e7b422becb9fb4848b9
Jerome <654321su@gmail.com>
2019-02-11 20:43:10 +0900

    #39 Add Users to DirectMessages Component, Show If Online/Offline

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -41,7 +41,7 @@ class UserPanel extends Component {
     const { displayName, photoURL } = this.state;
 
     return (
-      <Grid style={{ background: '#4c3c4c'}} >
+      <Grid style={{ background: '#4c3c4c'}}>
         <Grid.Column>
           <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
             {/* App Header */}
