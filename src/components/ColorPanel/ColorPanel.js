e1ff1a19e188449e1d0ddf2b534dfef477bca7a3
Jerome <654321su@gmail.com>
2019-02-21 10:41:20 +0900

    #51 Attach Chosen Colors to User Data

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
-import { Sidebar, Menu, Divider, Button, Modal, Icon, Label } from 'semantic-ui-react';
+import {
+  Sidebar,
+  Menu,
+  Divider,
+  Button,
+  Modal,
+  Icon,
+  Label,
+  Segment,
+} from 'semantic-ui-react';
 import { TwitterPicker } from 'react-color';
+import firebase from '../../firebase';
 
 class ColorPanel extends Component {
   state = {
-    modal: false
+    modal: false,
+    primary: '',
+    secondary: '',
+    user: this.props.currentUser,
+    usersRef: firebase.database().ref('users')
+  }
+
+  handleChangePrimary = color => this.setState({ primary: color.hex });
+
+  handleChangeSecondary = color => this.setState({ secondary: color.hex });
+
+  handleSaveColors = () => {
+    if (this.state.primary && this.state.secondary) {
+      this.saveColors(this.state.primary, this.state.secondary);
+    }
+  }
+
+  saveColors = (primary, secondary) => {
+    this.state.usersRef
+      .child(`${this.state.user.uid}/colors`)
+      .push()
+      .update({
+        primary,
+        secondary
+      })
+      .then(() => {
+        console.log('Colors added');
+        this.closeModal();
+      })
+      .catch(err => console.error(err));
   }
 
   openModal = () => this.setState({ modal: true });
@@ -12,7 +51,7 @@ class ColorPanel extends Component {
   closeModal = () => this.setState({ modal: false });
 
   render() {
-    const { modal } = this.state;
+    const { modal, primary, secondary } = this.state;
 
     return (
       <Sidebar
@@ -30,13 +69,17 @@ class ColorPanel extends Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Choose App Colors</Modal.Header>
           <Modal.Content>
-            <Label content="Primary Color" />
-            <TwitterPicker />
-            <Label content="Secondary Color" />
-            <TwitterPicker />
+            <Segment inverted>
+              <Label content="Primary Color" />
+              <TwitterPicker color={primary} onChange={this.handleChangePrimary} />
+            </Segment>
+            <Segment inverted>
+              <Label content="Secondary Color" />
+              <TwitterPicker color={secondary} onChange={this.handleChangeSecondary} />
+            </Segment>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSaveColors}>
               <Icon name="checkmark" /> Save Colors
             </Button>
             <Button color="red" inverted onClick={this.closeModal}>
