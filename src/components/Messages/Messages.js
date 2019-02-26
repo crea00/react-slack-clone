c91176bf3c8b207fa0bdbd18eb20ccb3c891d393
Jerome <654321su@gmail.com>
2019-02-26 10:22:22 +0900

    #57 Add Typing Component / Animation, Display in Messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -7,6 +7,7 @@ import firebase from '../../firebase';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import Typing from './Typing';
 
 class Messages extends Component {
   state = {
@@ -205,7 +206,12 @@ class Messages extends Component {
 
         <Segment>
           <Comment.Group className={progressBar ? 'messages__progress' : 'messages'}>
-            {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
+            {searchTerm
+              ? this.displayMessages(searchResults)
+              : this.displayMessages(messages)}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <span className="user__typing">douglas is typing</span> <Typing />
+            </div>
           </Comment.Group>
         </Segment>
 
